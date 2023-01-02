4fae58bfed89d0adc17a5bbbb4af5592fa3a03d0
Lucian Buzzo <lucian.buzzo@gmail.com>
2023-01-02 14:57:40 +0000

    Finish CLS example
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prisma + CLS
+
+This example shows how to use [Prisma](https://www.prisma.io/) with [CLS-hooked](https://www.npmjs.com/package/cls-hooked), allowing you to access the current request context in your Prisma middleware.
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { createNamespace } from "cls-hooked";
 
+const clsSession = createNamespace("server");
 const prisma = new PrismaClient();
 
+const middleware: Prisma.Middleware = async (params, next) => {
+  const ctx = clsSession.get("ctx");
+  console.log(
+    `user ${ctx.userId} is running action ${params.action} on model ${params.model}`
+  ); // e.g. `user 123 is running action create on model User`
+  return next(params);
+};
+
+prisma.$use(middleware);
+
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-    },
+  const user = await new Promise((resolve) => {
+    clsSession.run(async () => {
+      clsSession.set("ctx", {
+        userId: "123",
+      });
+
+      const response = await prisma.user.create({
+        data: {
+          name: "Alice",
+          email: "alice@prisma.io",
+        },
+      });
+      resolve(response);
+    });
   });
+
   console.log(user);
 }
 
